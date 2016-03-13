d0fde16d042c2ed4f9282cf257cc9fe5ffdeb183
Eugenio Leal <eugenio.leal.333@gmail.com>
2016-03-13 09:02:04 -0600

    Adds shooting bolts to the player. Complete Game Setup, Player and Camera.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,28 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public float tilt;
     public Boundary boundary;
-    
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            //GameObject clone = 
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject
+        }
     }
 
     void FixedUpdate()
