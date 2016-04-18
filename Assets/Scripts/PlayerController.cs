049ab4fe944645fb0c5fc8f950a33a74b5208690
Eugenio Leal <eugenio.leal.333@gmail.com>
2016-04-18 11:52:07 -0500

    Added myo funcionality to shoot the blaster gun when you close your fist. The changed code is in the player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
 [System.Serializable]
 public class Boundary
 {
@@ -21,6 +26,15 @@ public class PlayerController : MonoBehaviour {
 
     private float nextFire;
 
+    // Myo game object to connect with.
+    // This object must have a ThalmicMyo script attached.
+    public GameObject myo = null;
+
+    // The pose from the last update. This is used to determine if the pose has changed
+    // so that actions are only performed upon making them rather than every frame during
+    // which they are active.
+    private Pose _lastPose = Pose.Unknown;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -28,13 +42,29 @@ public class PlayerController : MonoBehaviour {
 
     void Update()
     {
-        
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
-        {
-            nextFire = Time.time + fireRate;
+        // Access the ThalmicMyo component attached to the Myo game object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
 
-            //GameObject clone = 
-            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject
+        // Check if the pose has changed since last update.
+        // The ThalmicMyo component of a Myo game object has a pose property that is set to the
+        // currently detected pose (e.g. Pose.Fist for the user making a fist). If no pose is currently
+        // detected, pose will be set to Pose.Rest. If pose detection is unavailable, e.g. because Myo
+        // is not on a user's arm, pose will be set to Pose.Unknown.
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+
+            /*Uncomment next line to enable myo pose.Comment out the line which has (Input.GetButton("Fire1") && Time.time > nextFire)
+            to disable myo and use the keyboard instead
+            if (thalmicMyo.pose == Pose.Fist) */
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+                nextFire = Time.time + fireRate;
+
+                //GameObject clone = 
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);// as GameObject
+            }
+
         }
     }
 
@@ -53,5 +83,19 @@ public class PlayerController : MonoBehaviour {
         );
 
         rb.rotation = Quaternion.Euler(0.0f,0.0f,rb.position.x * -tilt);
+    }
+
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction();
     }
 }
